e877ef1aad2e954d6281873d9056ad989c6ae634
谭美姿 <3289308758@qq.com>
2025-03-25 08:08:54 +0800

    hhh

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import routes from './baseRouteMap'
+import { createRouter, createWebHashHistory } from "vue-router";
+import routes from "./baseRouteMap";
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
+  history: createWebHashHistory(),
+  routes,
+});
 
-export default router
+export default router;
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/front1/",
+  build: { outDir: "docs" },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
-})
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
