b77c1b6ce09d987fe33913c62d9bb464730b418c
谭美姿 <3289308758@qq.com>
2025-03-25 08:13:06 +0800

    v

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/front1/",
+  base: "/tmz/",
   build: { outDir: "docs" },
   resolve: {
     alias: {
